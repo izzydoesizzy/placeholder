a5b30b9a08443512611f5db4d0cf891f40752b65
Iskender Piyale-Sheard <ipiyale@gmail.com>
2017-03-19 11:40:53 -0400

    try php index

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
+
+<?php
+/**
+ * Use your favorite autoload routine here.
+ */
+function __autoload($class)  
+{  
+  $filename = str_replace('\\', '/', $class) . '.php';  
+  @require_once 'classes/'.$filename;  
+}
+
+/**
+ * Define a variable or constant that points to the image folder
+ */
+define('IMAGEPATH', dirname(__FILE__).'/image_placeholders/');
+
+/**
+ * Initialize the PlaceHolder object
+ */
+$placeholder = new \seydoggy\PlaceHolder(IMAGEPATH);
+
+?>
+
 <!DOCTYPE html>
 
 <html lang="en">
@@ -379,24 +402,4 @@
     </script>
 </body>
 </html>
-
-<?php
-/**
- * Use your favorite autoload routine here.
- */
-function __autoload($class)  
-{  
-  $filename = str_replace('\\', '/', $class) . '.php';  
-  @require_once 'classes/'.$filename;  
-}
-
-/**
- * Define a variable or constant that points to the image folder
- */
-define('IMAGEPATH', dirname(__FILE__).'/image_placeholders/');
-
-/**
- * Initialize the PlaceHolder object
- */
-$placeholder = new \seydoggy\PlaceHolder(IMAGEPATH);
 
